00d762511a12939d588a1dc4eff977e11827a123
JFpro160 <jose.paca@utec.edu.pe>
2024-06-09 20:04:50 -0500

    SE PUEDE HACER LOGIN

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,25 +1,73 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { fetchLogin } from '../services/api';
 
 export const LoginForm = () => {
+  const navigate = useNavigate();
+
   const [formData, setFormData] = useState({
     email: '',
     password: '',
   });
 
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.password) {
+      setErrors({
+        email: !formData.email ? 'Se requiere correo.' : null,
+        password: !formData.password ? 'Se requiere contraseña.' : null,
+      });
+    } else {
+      try {
+        const response = await fetchLogin(formData);
+        if (response.status === 200) {
+          navigate('/dashboard');
+        }
+      } catch (error) {
+        if (error.response && error.response.status === 400) {
+          setErrors({
+            form: error.response.data,
+          });
+        } else {
+          setErrors({
+            form: 'Error desconocido, por favor intenta más tarde',
+          });
+        }
+      }
+    }
+  };
 
   return (
-    <section className="content-background">
+      <section className="content-background">
       <h1 className="title">Ingresar a Uber</h1>
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <div className="parameter">
           <label htmlFor="email">Correo</label>
-          <input type="email" name="email" id="email"/>
+          <input type="email" name="email" id="email"
+                 value={formData.email}
+                 onChange={handleChange}
+          />
+          {errors.email && <p className='text-red-500 text-sm'>{errors.email}</p>}
         </div>
         <div className="parameter">
           <label htmlFor="password">Contraseña</label>
-          <input type="password" name="password" id="password"/>
+          <input type="password" name="password" id="password"
+                 value={formData.password}
+                 onChange={handleChange}
+          />
+          {errors.password && <p className='text-red-500 text-sm'>{errors.password}</p>}
         </div>
         <button id="loginSubmit" className="button" type="submit">Iniciar Sesión</button>
+        {errors.form && <p className="text-red-500 text-sm mt-4">{errors.form}</p>}
       </form>
     </section>
   );
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,13 +3,127 @@ import { jwtDecode } from 'jwt-decode';
 
 const BACKEND_URL = 'http://127.0.0.1:8080'; // Spring Boot
 
-// Obtiene el endpoint que corresponde al token
 export const getRoleBasedOnToken = () => {
   const token = localStorage.getItem('token');
-  const decodedToken = jwtDecode(token);
-  return decodedToken.role;
+  if (!token) {
+    throw new Error('No token found');
+  }
+  try {
+    const decodedToken = jwtDecode(token);
+    return decodedToken.role;
+  } catch (error) {
+    throw new Error('Invalid token format');
+  }
+};
+
+export const isAuthenticated = () => {
+  const token = localStorage.getItem('token');
+  return token !== null;
 }
 
+
 export const fetchLogin = async (body) => {
-  
+  const response = await axios.post(`${BACKEND_URL}/auth/login`, {
+    email: body.email,
+    password: body.password
+  });
+  const token = response.data.token;
+  if (token) {
+    localStorage.setItem('token', token);
+  } else {
+    throw new Error('Token is not available');
+  }
+  return response;
 };
+
+export const fetchRegister = async (body) => {
+  const response = await axios.post(`${BACKEND_URL}/auth/register`, {
+    firstName: body.firstName,
+    lastName: body.lastName,
+    email: body.email,
+    password: body.password,
+    phone: body.phone,
+    isDriver: body.isDriver,
+    category: body.category,
+    vehicle: body.vehicle
+  });
+  const token = response.data.token;
+  localStorage.setItem('token', token);
+  return response;
+};
+
+export const getPassenger = async () => {
+  const token = localStorage.getItem('token');
+  const response = await axios.get(`${BACKEND_URL}/passenger/me`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return response.data;
+};
+
+export const getDriver = async () => {
+  const token = localStorage.getItem('token');
+  const response = await axios.get(`${BACKEND_URL}/driver/me`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return response.data;
+}
+
+export const getRidesByUser = async (page, size) => {
+  const token = localStorage.getItem('token');
+  const response = await axios.get(`${BACKEND_URL}/ride/user`, {
+    headers: { Authorization: `Bearer ${token}` },
+    params: { page, size }
+  });
+  return response.data;
+}
+
+export const updatePassenger = async (body) => {
+  const token = localStorage.getItem('token');
+  await axios.patch(`${BACKEND_URL}/passenger/me`, {
+    firstName: body.firstName,
+    lastName: body.lastName,
+    phoneNumber: body.phoneNumber
+  }, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+}
+
+export const deletePassenger = async (id) => {
+  const token = localStorage.getItem('token');
+  await axios.delete(`${BACKEND_URL}/passenger/${id}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+}
+
+export const updateDriverInfo = async (id, body) => {
+  const token = localStorage.getItem('token');
+  await axios.patch(`${BACKEND_URL}/driver/${id}`, {
+    firstName: body.firstName,
+    lastName: body.lastName,
+    phoneNumber: body.phoneNumber
+  }, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return "Driver info updated successfully";
+}
+
+export const updateDriverCar = async (id, body) => {
+  const token = localStorage.getItem('token');
+  await axios.patch(`${BACKEND_URL}/driver/${id}/car`, {
+    brand: body.brand,
+    model: body.model,
+    licensePlate: body.licensePlate,
+    fabricationYear: body.fabricationYear,
+    capacity: body.capacity
+  }, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return "Driver car updated successfully";
+}
+
+export const deleteDriver = async (id) => {
+  const token = localStorage.getItem('token');
+  await axios.delete(`${BACKEND_URL}/driver/${id}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+}
